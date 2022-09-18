cb051f336308d24dd6456f6e01ef7125e1bda156
mechanicworld <47930174+mechanicworld@users.noreply.github.com>
2022-09-18 21:41:54 +0300

    Add Realtime color app

---
+++ mid/realtime-colors/client/src/App.js
@@ -0,0 +1,26 @@
+
+import './App.css';
+import {useEffect, useState} from 'react'
+import Palette from './components/Palette';
+import {init,subscribe} from './socketApi'
+function App() {
+const [activeColor, setActiveColor] = useState('#282c34')
+  useEffect(() => {
+    init()  
+    subscribe((color)=>{
+      setActiveColor(color)
+    } )
+    // return () => {
+    //   second
+    // }
+  }, [])
+  
+  return (
+    <div className="App" style={{backgroundColor:`${activeColor}`}}>
+      <h1>{activeColor}</h1>
+      <Palette activeColor={activeColor}/>
+    </div>
+  );
+}
+
+export default App;
